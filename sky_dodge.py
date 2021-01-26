c19150150a906281a7c819b093baa5fb72286d8a
adrian mohabir <mohabiradrian@gmail.com>
2021-01-25 20:14:01 -0500

    import pygame create new file import pygame.locals

---
+++ sky_dodge.py
@@ -0,0 +1,15 @@
+# import pygame module
+import pygame
+
+# import pygame.locals for easier access to key coordinates
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+# Initialize pygame 
+pygame.init()
