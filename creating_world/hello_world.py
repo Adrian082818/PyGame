0b1839af7e90da098821ab2b90dc11d68d208bee
adrian mohabir <mohabiradrian@gmail.com>
2021-01-28 21:30:23 -0500

    flip the image when moving character left

--- creating_world/hello_world.py
+++ creating_world/hello_world.py
@@ -25,18 +25,22 @@ background_img = pygame.image.load('img/sky.png')
 class Player():
     def __init__(self, x, y):
         self.images_right = []
+        self.images_left = []
         self.index = 0
         self.counter = 0
         for num in range(1, 5):
             img_right = pygame.image.load(f'img/guy{num}.png')
             img_right = pygame.transform.scale(img_right, (40, 80))
+            img_left = pygame.transform.flip(img_right, True, False)
             self.images_right.append(img_right)
+            self.images_left.append(img_left)
         self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
+        self.direction = 0
 
     def update(self):
         dx = 0 
@@ -53,13 +57,18 @@ class Player():
         if key[pygame.K_LEFT]:
             dx -= 5
             self.counter += 1 
+            self.direction = -1
         if key[pygame.K_RIGHT]:
             dx += 5
             self.counter += 1
+            self.direction = 1
         if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
             self.counter = 0
             self.index = 0
-            self.image = self.images_right[self.index]
+        if self.direction == 1:
+                self.image = self.images_right[self.index]
+        if self.direction == -1:
+                self.image = self.images_left[self.index]
 
 
 
@@ -69,7 +78,10 @@ class Player():
             self.index += 1
             if self.index >= len(self.images_right):
                 self.index = 0
-            self.image = self.images_right[self.index]
+            if self.direction == 1:
+                self.image = self.images_right[self.index]
+            if self.direction == -1:
+                self.image = self.images_left[self.index]
 
         # add gravity
         self.vel_y += 1 
