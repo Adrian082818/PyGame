152925ce70b1f16215b695946562417cda49950d
adrian mohabir <mohabiradrian@gmail.com>
2021-02-02 19:13:37 -0500

    draw coins onto the screen

--- creating_world/hello_world.py
+++ creating_world/hello_world.py
@@ -232,6 +232,9 @@ class World():
                 if tile == 6:
                     lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
                     lava_group.add(lava)
+                if tile == 7:
+                    coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+                    coin_group.add(coin)
                 if tile == 8:
                     exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
                     exit_group.add(exit)
@@ -329,6 +332,7 @@ while run:
 
         blob_group.draw(screen)
         lava_group.draw(screen)
+        coin_group.draw(screen)
         exit_group.draw(screen)
 
         game_over = player.update(game_over)
